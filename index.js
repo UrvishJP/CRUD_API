fa613433f46825a53b54ec0dd29d6549144b30a2
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-05-29 17:26:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+
+// Load environment variables
+require("dotenv").config();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from CRUD API");
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
